6ae4d1c0f4db898f0c3acfeb0cfb3a72840467e1
PoojaMaurya-WebDev <poojamaurya8237@gmail.com>
2025-05-01 14:02:22 +0000

    01-basic

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,14 @@
+const accountID = 144334
+let accountEmail = "pooja123@gmail.com"
+var accountPassword = "123476"
+accountCity = "mumbai"
+
+// accountID = 2 // not allowed
+
+accountEmail = "p@gmail.com"
+accountPassword = "1211"
+accountCity = "jaunpur"
+
+console.log(accountID);
+
+console.table([accountEmail, accountID, accountPassword, accountCity])
